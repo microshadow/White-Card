5f71c46c94b9782f58a9082bed863925d94a647b
microshadow <72engineers@gmail.com>
2019-04-14 11:34:17 -0400

    save task items to mongoDB

--- front-end/todo-list/app.js
+++ front-end/todo-list/app.js
@@ -1,13 +1,34 @@
 const log = console.log;
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-let tasks = [];
+// connect to mongo server
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser:true});
+// create Schema
+const ItemSchema = new mongoose.Schema({
+    name: String,
+});
+// create model
+const Item = mongoose.model("Item", ItemSchema);
+// create document
+const item1 = new Item({
+    name: "学习！"
+});
+
+const item2 = new Item({
+    name: "看球：利物浦 vs 切尔西"
+});
+
+const item3 = new Item({
+    name: "健身：卧推，深蹲各7组，无氧30分钟"
+});
+
+let tasks = [item1, item2, item3];
 
 app.get("/", (req,res) => {
     const today = new Date();
@@ -18,12 +39,31 @@ app.get("/", (req,res) => {
     };
     let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {day: day, tasks: tasks});
+    if (tasks.length === 0){
+        // save to mongoDB
+        Item.insertMany(tasks, err =>{
+            if (err){
+                log(err);
+            } else {
+                log("added new task items to todolist")
+            }
+        });
+        res.redirect("/")
+    } else {
+        Item.find({}, (err, found) =>{
+            res.render("list", {day: day, tasks: found});
+        });
+    }
+
+    // res.render("list", {day: day, tasks: tasks});
 });
 
 app.post("/", (req,res) => {
     const newTask = req.body.newTask;
-    tasks.push(newTask);
+    const newItem = new Item({name:newTask});
+    newItem.save();
+
+    //tasks.push(newTask);
     res.redirect("/");
 });
 
